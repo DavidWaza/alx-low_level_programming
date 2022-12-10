7249d6fd79042ec1ddbda14fefd431d8686b5230
Idemeto emediong <davidyega69@gmail.com>
2022-12-09 18:12:53 -0800

    print base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n = '0';
+	char hex = 'a';
+
+	while (n <= '9')
+	{
+		putchar(n);
+		n++;
+	}
+	while (hex <= 'f')
+	{
+		putchar(hex);
+		hex++;
+	}
+	putchar('\n');
+
+	return (0);
+}
