5d403f2a099c42ef68f84a43682ed9770d3e2e2e
Idemeto emediong <davidyega69@gmail.com>
2022-12-13 13:36:45 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints _putchar followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char text[8] = "_putchar";
+	int i = 0;
+
+	for (i = 0; i < 8; i++)
+	{
+		_putchar(text[i]);
+	}
+	_putchar("\n");
+
+	return (0);
+}
